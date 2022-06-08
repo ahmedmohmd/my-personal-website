468a44882307ec0366e6e4cae4498cf67800d25b
Ahmed Mohamed <er.ahmed.3301@gmail.com>
2022-06-08 17:13:47 +0200

    add app to heroku

--- src/components/contact/Form.jsx
+++ src/components/contact/Form.jsx
@@ -147,7 +147,7 @@ function Form() {
       setErrors(result);
     } else {
       axios
-        .post("http://localhost:5000/contactMe", formData)
+        .post("https://ahmedmohmd-website.herokuapp.com/contactMe", formData)
         .then(() => {
           swal("Success", "Thanks for Contacting", "success").then(
             () => (window.location = "/")
