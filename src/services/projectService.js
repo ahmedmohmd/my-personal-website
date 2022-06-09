52a7da682f306c241587659b9d1db794e3418849
Ahmed Mohamed <er.ahmed.3301@gmail.com>
2022-06-09 17:19:00 +0200

    add some projects

--- src/services/projectService.js
+++ src/services/projectService.js
@@ -13,6 +13,67 @@ const projects = [
     githubUrl: "https://github.com/ahmedmohmd/weather-app   ",
     livePreviewUrl: "https://weather-app-beige-nine.vercel.app/",
   },
+  {
+    id: 3,
+    title: "Squid Game",
+    cover: "/images/squid-game.png",
+    githubUrl: "https://github.com/ahmedmohmd/squid-game/tree/main",
+    livePreviewUrl: "https://squid-game-4zseic9ey-a7m3d000.vercel.app/#/",
+  },
+  {
+    id: 4,
+    title: "Geniuses",
+    cover: "/images/geniuses.png",
+    githubUrl: "https://github.com/ahmedmohmd/L--Geniuses/tree/main",
+    livePreviewUrl: "https://geniuses.vercel.app/",
+  },
+  {
+    id: 5,
+    title: "Pomodoro App",
+    cover: "/images/pomodoro-app.png",
+    githubUrl: "https://github.com/ahmedmohmd/pomodoro-app/tree/main",
+    livePreviewUrl: "https://pomodoro-app-phi.vercel.app/",
+  },
+  {
+    id: 6,
+    title: "Time Tracking App",
+    cover: "/images/time-tracking-app.png",
+    githubUrl:
+      "https://github.com/ahmedmohmd/time-tracking-dashboard/tree/main",
+    livePreviewUrl:
+      "https://time-tracking-dashboard-seven-zeta.vercel.app/#/weekly",
+  },
+  {
+    id: 7,
+    title: "Quizz App",
+    cover: "/images/quizz-app.png",
+    githubUrl: "https://github.com/ahmedmohmd/quizz-app/tree/main",
+    livePreviewUrl: "https://quizz-app-eta.vercel.app/",
+  },
+  {
+    id: 8,
+    title: "Calculator App",
+    cover: "/images/calculator-app.png",
+    githubUrl: "https://github.com/ahmedmohmd/calculator-app/tree/main",
+    livePreviewUrl: "https://calculator-six-rosy.vercel.app/",
+  },
+  {
+    id: 9,
+    title: "Stripe Payments",
+    cover: "/images/stripe-payments-app.png",
+    githubUrl:
+      "https://github.com/ahmedmohmd/-stripe-payment-frontend/tree/main",
+    livePreviewUrl: "https://stripe-payment-frontend.vercel.app/",
+  },
+  {
+    id: 10,
+    title: "Sunnyside Landing Page",
+    cover: "/images/sunnyside-app.png",
+    githubUrl:
+      "https://github.com/ahmedmohmd/Sunnyside-Agency-Landing-Page/tree/main",
+    livePreviewUrl:
+      "https://sunnyside-agency-landing-page-eosin.vercel.app/#/body",
+  },
 ];
 
 export default projects;
