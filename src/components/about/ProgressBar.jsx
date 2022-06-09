6b6ebcac10dc1809b5fc1a6e8fc8cd40e23090f3
Ahmed Mohamed <er.ahmed.3301@gmail.com>
2022-06-09 12:05:28 +0200

    edit progres bar colors

--- src/components/about/ProgressBar.jsx
+++ src/components/about/ProgressBar.jsx
@@ -4,21 +4,21 @@ function ProgressBar({ skillName, progress, width }) {
     <div className="relative w-full pt-1">
       <div className="flex items-center justify-between mb-2">
         <div>
-          <span className="inline-block px-2 py-1 text-xs font-semibold text-purple-600 uppercase bg-purple-200 rounded-full dark:text-white dark:bg-purple-500/50">
+          <span className="inline-block px-2 py-1 text-xs font-semibold text-blue-600 uppercase bg-blue-200 rounded-full dark:text-white dark:bg-blue-500/50">
             {skillName}
           </span>
         </div>
         <div className="text-right">
-          <span className="inline-block text-xs font-semibold text-purple-600">
+          <span className="inline-block text-xs font-semibold text-blue-600 dark:text-gray-200">
             {progress}%
           </span>
         </div>
       </div>
-      <div className="flex h-2 mb-4 overflow-hidden text-xs bg-purple-200 rounded">
+      <div className="flex h-2 mb-4 overflow-hidden text-xs bg-blue-200 rounded">
         <div
           className={
             width +
-            " shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-gradient-to-l from-blue-500 to-purple-600"
+            " shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-500"
           }
         ></div>
       </div>
