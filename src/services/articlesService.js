659c098283e27d200269528fd24b95d87302fdb7
Ahmed Mohamed <er.ahmed.3301@gmail.com>
2022-06-26 09:50:09 +0200

    refactor code

--- src/services/articlesService.js
+++ src/services/articlesService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import config from "../config/config.json";
 
 const getArticles = () => {
-  return axios.get("https://dev.to/api/articles?username=ahmedmohmd&state=all");
+  return axios.get(config.devDotToArticlesApiEndPoint);
 };
 
 export default getArticles;
